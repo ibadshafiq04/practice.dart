c4528af1e2befb5e6cfa9a5a1756d4ef9acac5ff
ibadshafiq04 <ibadshafiq04@gmail.com>
2025-04-24 00:17:47 +0500

    add if_else.dart & practice.dart

---
+++ practice.dart
@@ -0,0 +1,36 @@
+void main(){
+
+// bool mathques = 45 < 30;
+// print(mathques);
+
+// int age = 1;
+// bool result = age != 1;
+// print(result);
+
+// String email = "abc1@gmail.com";
+// String password = "ibad123";
+// bool condition =email=="abc1@gmail.com"&&password=="ibad123";
+// print(condition); 
+
+int abc = 23;
+int o = 34;
+int i = 45;
+int f = 30;
+// int e = ?;
+// int c = ?;
+bool e = abc<o || !(o>abc);
+// e = false || !(false)
+// e = false ||   true
+// e = true
+// print(e);
+bool c = !e || i!=f;
+//      !true || true
+//      false || true
+// c =  true
+// print(c);
+
+
+
+
+
+}
