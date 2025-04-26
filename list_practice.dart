4a6178e1073a94e7f296e5fe6ee46db9752128e7
ibadshafiq04 <ibadshafiq04@gmail.com>
2025-04-26 21:41:52 +0500

    update list_practice.dart

--- list_practice.dart
+++ list_practice.dart
@@ -1,11 +1,19 @@
 void main(){
 
-int number = 21;
-if (number%5==0) {
-  print("number is divisible by zero");
+int number = 22;
+if (number%5==0 || number%2==0) {
+if (number%5==0 && number%2==0) {
+print("number is divisible by both");  
+}
+else if(number%2==0){
+print("number is divisible by 2");
+}
+else{
+print("number is divisible by 5");
+}
 }
 else {
-print("number is not divisible by 0");
+print("number is not divisible by 5 or 2");
 }
 
 
